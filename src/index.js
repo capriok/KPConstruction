9efcefa6819acdc13bf995fc3b8ea508fcc8ab7f
Kyle Caprio <capriok7@yahoo.com>
2020-05-24 19:48:00 -0700

    touches and favicon

--- src/index.js
+++ src/index.js
@@ -3,13 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './App.scss';
 
-export default function Index() {
-  return (
-    <>
-      <App />
-    </>
-  );
-}
-
-
-ReactDOM.render(<Index />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
