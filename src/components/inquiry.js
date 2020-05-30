c14240a2c8ca17b2b9dd21f22ffaa9475fcdb85f
Kyle Caprio <capriok7@yahoo.com>
2020-05-29 17:46:50 -0700

    submit btn fix

--- src/components/inquiry.js
+++ src/components/inquiry.js
@@ -24,38 +24,40 @@ const Inquiry = ({
             <h1>{inquiryState.statusTitleMessage}</h1>
             <h1 className="sub">{inquiryState.statusSubMessage}</h1>
           </div>
-          : <form onSubmit={(e) => { submitEmail(e) }}>
-            <input type="text"
-              placeholder="Enter Name (Required)"
-              value={inquiryForm.name}
-              onChange={(e) => setInquiryForm({ ...inquiryForm, name: e.target.value })}
-            />
-            <input type="text"
-              placeholder="Enter Email (Required)"
-              value={inquiryForm.email}
-              onChange={(e) => setInquiryForm({ ...inquiryForm, email: e.target.value })}
-            />
-            <textarea type="text" rows="5"
-              placeholder="Message Body (Required)"
-              value={inquiryForm.body}
-              onChange={(e) => setInquiryForm({ ...inquiryForm, body: e.target.value })}
-            />
-            <div className="select-opt">
-              <p className="title"><span>How did you hear about us?</span><span>Optional</span></p>
-              <select
-                onChange={(e) => setInquiryForm({ ...inquiryForm, referral: e.target.value })}>
-                <option value=""></option>
-                <option value="Newspaper">Newspaper</option>
-                <option value="Radio">Radio</option>
-                <option value="Friend">Friend</option>
-                <option value="Facebook">Facebook</option>
-                <option value="Instagram">Instagram</option>
-              </select>
-            </div>
-            <input type="submit" value="Submit"
+          : <>
+            <form id="inquiryForm" onSubmit={(e) => { submitEmail(e) }}>
+              <input type="text"
+                placeholder="Enter Name (Required)"
+                value={inquiryForm.name}
+                onChange={(e) => setInquiryForm({ ...inquiryForm, name: e.target.value })}
+              />
+              <input type="email"
+                placeholder="Enter Email (Required)"
+                value={inquiryForm.email}
+                onChange={(e) => setInquiryForm({ ...inquiryForm, email: e.target.value })}
+              />
+              <textarea type="text" rows="5"
+                placeholder="Message Body (Required)"
+                value={inquiryForm.body}
+                onChange={(e) => setInquiryForm({ ...inquiryForm, body: e.target.value })}
+              />
+              <div className="select-opt">
+                <p className="title"><span>How did you hear about us?</span><span>Optional</span></p>
+                <select
+                  onChange={(e) => setInquiryForm({ ...inquiryForm, referral: e.target.value })}>
+                  <option value=""></option>
+                  <option value="Newspaper">Newspaper</option>
+                  <option value="Radio">Radio</option>
+                  <option value="Friend">Friend</option>
+                  <option value="Facebook">Facebook</option>
+                  <option value="Instagram">Instagram</option>
+                </select>
+              </div>
+            </form>
+            <button form="inquiryForm"
               className={inquiryState.submitLoading ? "submit-btn dis" : "submit-btn"}
-            />
-          </form>
+            >Submit</button>
+          </>
         }
       </div>
     </>
