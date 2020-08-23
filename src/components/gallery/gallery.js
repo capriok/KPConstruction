1366e4912a5da245ba77fc3e6b30afa5fece3ff3
Kyle Caprio <capriok7@yahoo.com>
2020-08-23 14:38:09 -0700

    no warns

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,3 +1,4 @@
+/*eslint react-hooks/exhaustive-deps: "off"*/
 import React from 'react'
 import AliceCarousel from 'react-alice-carousel'
 import 'react-alice-carousel/lib/alice-carousel.css'
--- src/hooks/lockbody.js
+++ src/hooks/lockbody.js
@@ -1,3 +1,4 @@
+/*eslint react-hooks/exhaustive-deps: "off"*/
 import React from 'react'
 
 export default function useLockBodyScroll() {
